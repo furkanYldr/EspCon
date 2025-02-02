c60e8837e0fd32abf049120701cc4ff6113fce63
furkanYldr <149538417+furkanYldr@users.noreply.github.com>
2025-02-02 17:02:47 +0300

    fix conflict pacman

--- pacman/pacman.h
+++ pacman/pacman.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef PACMAN_H
 #define PACMAN_H
 
@@ -68,74 +67,4 @@ extern bool ghostINKY;
 extern  bool CATCH;
 extern int timer, prevTimer;
 
-
-=======
-#ifndef PACMAN_H
-#define PACMAN_H
-
-#include <TFT_eSPI.h>
-#include <Arduino.h>
-#include "resource.h"
-#include "ghosts.h"
-#include "tabs.h"
-#include <vector>
-
-using namespace std;
-
-// Animasyon ve yön enumları
-enum animDirection {
-  AUP,
-  ADOWN,
-  ALEFT,
-  ARIGHT
-};
-enum pacmanDirection {
-  PUP,
-  PDOWN,
-  PLEFT,
-  PRIGHT,
-  PEMPTY
-};
-
-// Pacman yapısı
-struct STRUCTPACMAN {
-  int px, py;
-  int pvx, pvy;
-  bool up, down, front, back, open;
-  pacmanDirection wantedDirection;
-  animDirection ANIM;
-};
-
-// Fonksiyon prototipleri
-void gameManager();
-void pacmanSetup();
-void pacmanUpdate();
-void addGhost(int col);
-void drawInit();
-void drawMaze();
-void drawPacman(uint8_t matrix[10][10]);
-void canMove();
-void pacMOVEMENT();
-void alignToGrid();
-void collectFood(int row, int col);
-void monitorMemory();
-void healthTracker();
-void gameSetup();
-
-// Global değişkenler
-extern TFT_eSPI tft;
-extern TFT_eSprite img;
-extern STRUCTPACMAN pacman;
-
-extern bool paused;
-extern bool UP, DOWN, LEFT, RIGHT, SELECT;
-
-extern const int xPadding, yPadding;
-
-extern int ghostAnim;
-
-extern  bool CATCH;
-extern int timer, prevTimer;
-
->>>>>>> d3809c2514bf5a622f3d718869e316c3221eedf2
 #endif  // PACMAN_H
--- pacman/tabs.h
+++ pacman/tabs.h
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #ifndef TABS_H
 #define TABS_H
 #include "pacman.h"
@@ -20,29 +20,4 @@ void startAnim();
 
 
 
-
-=======
-#ifndef TABS_H
-#define TABS_H
-#include "pacman.h"
-
-enum gameStateEnum{
-
- start,
- game,
- gameOver,
- 
-
-};
-
-extern gameStateEnum gameState;
-
-void startTAB();
-void gameOverTAB();
-void startAnim();
-
-
-
-
->>>>>>> d3809c2514bf5a622f3d718869e316c3221eedf2
 #endif 
